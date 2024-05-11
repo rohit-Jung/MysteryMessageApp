58702dd35ddd7110bb501f894a26661db7c25a88
rohit-Jung <157964658+rohit-Jung@users.noreply.github.com>
2024-05-11 16:30:44 +0545

    error fixed in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,19 +7,20 @@ import { getToken } from "next-auth/jwt";
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
+  console.log("this is url", request.url);
 
   if (
     token &&
     (url.pathname.startsWith("/signin") ||
       url.pathname.startsWith("/signup") ||
       url.pathname.startsWith("/verify") ||
-      url.pathname.startsWith("/"))
+      url.pathname === "/")
   ) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
   if (!token && url.pathname.startsWith("/dashboard")) {
-    return NextResponse.redirect(new URL("/sign-in", request.url));
+    return NextResponse.redirect(new URL("/signin", request.url));
   }
 
   return NextResponse.next();
